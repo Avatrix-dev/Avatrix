9587d8ddf73c0a2c4e0dea1e13a9d434ca77223d
Avatrix <125829057+Avatrix-dev@users.noreply.github.com>
2024-05-09 13:23:17 +0100

    Create test.php

---
+++ php/test.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Avatrix</title>
+</head>
+
+<body>
+ <h1>My php test page</h1>
+your name is : <?php echo $_POST["name"] ?> <br>
+your number is : <?php echo $_POST["number"] ?> 
+</body>
+
+</html>
